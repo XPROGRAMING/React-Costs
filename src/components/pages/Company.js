ded2c59da8c84922213eab60fb18813e05044b3f
Michael Henrique <michaelhenrique@Air-de-Michael.local>
2024-03-29 18:01:35 -0300

    Projeto crud

---
+++ src/components/pages/Company.js
@@ -0,0 +1,9 @@
+function Company() {
+    return(
+        <div>
+            <h2>Pagina comany</h2>
+        </div>
+    )
+}
+
+export default Company
---
+++ src/components/pages/NewProject.js
@@ -0,0 +1,9 @@
+function NewProject() {
+    return(
+        <div>
+            <h2>Pagina de Projetos</h2>
+        </div>
+    )
+}
+
+export default NewProject
